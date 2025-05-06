bff33e84e3495a087b76042936dc153f6f0a718f
Данила Сергеевич <LukaDanS@yandex.ru>
2025-05-06 23:53:17 +0300

    Добавьте файлы проекта.

---
+++ MetodiAndPolomorfizm/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// перезагрузка методов
+namespace MetodiAndPolomorfizm
+{
+    internal class SurfaceAreaCalculator
+    {
+        public static double CalculateSurfaceArea(double a)
+        {
+            return 6 * Math.Pow(a, 2);
+        }
+        public static double CalculateSurfaceArea(double a, double b, double c)
+        {
+            return 2 * (a * b + a * c + b * c);
+        }
+        public static double CalculateSurfaceArea(double r, double h)
+        {
+            return 2 * Math.PI * r * (r + h);
+        }
+        public static double CalculateSurfaceAreaSphere(double r)
+        {
+            return 4 * Math.PI * Math.Pow(r, 2);
+        }
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            double cubeSide = 3;
+            double cubeSurfaceArea = SurfaceAreaCalculator.CalculateSurfaceArea(cubeSide);
+            Console.WriteLine($"Площадь поверхности куба: {cubeSurfaceArea}");
+
+            double lenght = 2, width = 3, height = 4;
+            double rectangularPrismSurfaceArea = SurfaceAreaCalculator.CalculateSurfaceArea(lenght, width, height);
+            Console.WriteLine($"Площадь поверхности прямоугольного параллелепипеда: {rectangularPrismSurfaceArea}");
+
+            double radius = 5; double cylinderHeight = 10; 
+            double cylinderSurfaceArea = SurfaceAreaCalculator.CalculateSurfaceArea(radius, cylinderHeight);
+            Console.WriteLine($"Пощадь поверхности цилиндра: {cylinderSurfaceArea}");
+
+            double sphereRadius = 7;
+            double sphereSurfaceArea = SurfaceAreaCalculator.CalculateSurfaceArea(sphereRadius);
+            Console.WriteLine($"Площадь поверхности шара: {sphereSurfaceArea}");
+        }
+    }
+}
---
+++ MetodiAndPolomorfizm/Shape.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// полиморфизм
+namespace MetodiAndPolomorfizm
+{
+    abstract class Shape
+    {
+        public abstract double CalculateSurfaceArea();
+    }
+    class Cube : Shape
+    {
+        private double side;
+
+        public Cube(double side)
+        {
+            this.side = side;
+        }
+        public override double CalculateSurfaceArea()
+        {
+            return 6 * Math.Pow(side, 2);
+        }
+    }
+    class RectangularPrism : Shape
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public RectangularPrism(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+        public override double CalculateSurfaceArea()
+        {
+            return 2 * (length * width + length * height + width * height);
+        }
+    }
+
+    class Cylinder : Shape
+    {
+        private double radius;
+        private double height;
+
+        public Cylinder(double radius, double height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+        public override double CalculateSurfaceArea()
+        {
+            return 2 * Math.PI * radius * (radius + height);
+        }
+    }
+    class Sphere : Shape
+    {
+        private double radius;
+
+        public Sphere(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateSurfaceArea()
+        {
+            return 4 * Math.PI * Math.Pow(radius, 2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[]
+            {
+                new Cube(3),
+                new RectangularPrism(2, 3, 4),
+                new Cylinder(5, 10),
+                new Sphere(6)
+            };
+            foreach(var shape in shapes)
+            {
+                Console.WriteLine($"Площадь поверхности: {shape.CalculateSurfaceArea()}");
+            }
+        }
+    }
+}
